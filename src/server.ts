11ef9b5c4c24f01f15610cd823b7e90e0432c8e3
cortella <nicolau.cortella@gmail.com>
2022-07-03 03:46:44 -0300

    document application with swagguer

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
-
+const door = 3333;
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
-app.listen(3333, () => console.log("running!"));
+app.listen(3333, () =>
+  console.log(
+    `\nAPI URL: http://localhost:${door}
+    \nSwagger: http://localhost:${door}/api-docs`
+  )
+);
